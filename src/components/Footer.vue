f162c24b7f5ef853285eda47c6af89333eb2dddc
minho <radlohead@naver.com>
2019-05-12 17:27:03 +0900

    vue todo file add

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <div>Footer</div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
---
+++ src/containers/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <Header></Header>
+    <TodoList></TodoList>
+    <Footer></Footer>
+  </section>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import TodoList from "@/components/TodoList.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    TodoList,
+    Footer
+  }
+};
+</script>
+
