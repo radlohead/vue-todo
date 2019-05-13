5b830b58b9c82b3f739d210108e252ad4562cc8d
minho <radlohead@naver.com>
2019-05-14 08:03:58 +0900

    input 값 입력후 엔터키 입력시 list 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     Header
-    <input type="text" ref="todo">
+    <input type="text" ref="todo" v-on:keyup.enter="addTodo">
     <button v-on:click="addTodo">add</button>
   </div>
 </template>
