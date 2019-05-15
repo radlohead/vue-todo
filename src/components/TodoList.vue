18397b666f62b2c0852024620bfc6dee67563638
minho <radlohead@naver.com>
2019-05-16 08:06:17 +0900

    todo 삭제 기능 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,10 @@
   <section>
     Todo list
     <ul>
-      <li v-for="(todo, index) in todos" v-bind:key="`${todo}-${index}`">{{ todo }}</li>
+      <li v-for="(todo, index) in todos" v-bind:key="`${todo}-${index}`">
+        <span>{{ todo }}</span>
+        <button v-on:click="removeElement(todo)">X</button>
+      </li>
     </ul>
   </section>
 </template>
@@ -14,6 +17,12 @@ export default {
     return {
       uniqueKey: new Date().getTime()
     };
+  },
+  methods: {
+    removeElement(text) {
+      const index = this.todos.findIndex(v => v === text);
+      this.todos.splice(index, 1);
+    }
   }
 };
 </script>
